9b986659e3ed95a2ae6d08328fed615d51ff5fa3
tatia-a <108894837+tatia-a@users.noreply.github.com>
2022-07-31 16:25:58 +0300

    Добавлен уровень

--- Boost Game/Assets/Rocket.cs
+++ Boost Game/Assets/Rocket.cs
@@ -7,7 +7,8 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource audioSource;
-    public float rotationSpeed = 0.3f;
+    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] float thrustSpeed = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,18 +24,36 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void Rotate()
     {
         // Повороты
         rigidBody.freezeRotation = true;
 
+        float rotationThisFrame = rotationSpeed * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0, 0, rotationSpeed);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0, 0, -rotationSpeed);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false;
@@ -43,9 +62,11 @@ public class Rocket : MonoBehaviour
     private void Thrust()
     {
         // Подъём на пробел (со звуком)
+        float thrustThisFrame = thrustSpeed * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!audioSource.isPlaying)
                 audioSource.Play();
         }
